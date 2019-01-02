322aed222e25561513a6b80d435f8f56a1dfbc3a
Masetty Abhishek <abhishekmashetty97@gmail.com>
2019-01-02 17:55:50 +0530

    Added few additional features for listing the users with respective articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,30 @@ class UsersController < ApplicationController
 		else
 			render 'new'
 		end
+	end
 
+	def edit
+		@user=User.find(params[:id])
 
+	end
+
+	def update
+		@user=User.find(params[:id])
+		if @user.update(user_params)
+			flash[:success]="Details have been successfully Updated!"
+			redirect_to articles_path
+		else
+			render 'edit'
+		end
+	end
+
+	def show
+		@user=User.find(params[:id])
+		
+	end
+
+	def index
+		@users=User.all
 	end
 
 	private
